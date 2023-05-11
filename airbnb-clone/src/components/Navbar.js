476118d31da9348d77d3ce183f8bc54af877092d
Eldar Heleg <eldar.heleg@gmail.com>
2023-05-11 13:32:46 +0200

    navbar component

---
+++ airbnb-clone/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import logo from "../images/airbnb-logo.png"
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <img src={logo} alt="airbnb" />
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
